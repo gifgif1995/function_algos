172b24c2aaf0efffa14080e608c42042a9b2ae45
bryannasav <bryanna.savelesky@gmail.com>
2020-02-20 17:51:06 -0800

    Fix typo

--- 3_shift_left.js
+++ 3_shift_left.js
@@ -1,7 +1,7 @@
 // Given an array, shift all values one position to the left.  Change the final position to 0. Return the changed array
 // Predicted Output: shiftLeft([1,2,3,4,5]) should return [2,3,4,5,0];
 
-function shitftLeft(arr){
+function shiftLeft(arr){
     // YOUR CODE HERE
 }
 var resultArr = shiftLeft([1,2,3,4,5]);
